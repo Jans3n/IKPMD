988f461c6f611d6d3532c9fde65ff73509998cd9
Jans3n <jans3nsu@gmail.com>
2022-06-27 22:47:45 +0200

    UI design login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      // backgroundColor: ,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "MyMoney",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Login to MyMoney",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 44,
+            ),
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Email address",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            RichText(
+              text: TextSpan(
+                text: "Create a account",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                  Navigator.pushNamed(context, '/main');
+                  }
+              ),
+            ),
+            const SizedBox(
+              height: 88,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Color(0xFF00AB95),
+                elevation: 0,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
